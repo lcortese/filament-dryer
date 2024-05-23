09403ef16b15d2bc14f5cb61c682929230664066
Leandro Cortese <leandrocortese@gmail.com>
2024-05-23 06:27:20 +0000

    Implemented config

--- main/Menu.h
+++ main/Menu.h
@@ -1,8 +1,10 @@
 class Menu {
-  static const uint8_t LIST_LENGTH = 2;
-  const String LIST[LIST_LENGTH] = { "Configuration", "Back" };
+  static const uint8_t LIST_LENGTH = 3;
+  String LIST[LIST_LENGTH] = { "Configuration", "Back", "Start" };
+  String formattedList[LIST_LENGTH];
   Display& mainDisplay;
   Encoder& mainEncoder;
+  Config& configMenu;
   void (*goBack)();
   void (*goToConfig)();
   List menuList;
@@ -16,17 +18,27 @@ class Menu {
     Menu(
       Display& display,
       Encoder& encoder,
+      Config& config,
       void (*onGoBack)(),
       void (*onGoToConfig)()
     ) :
       mainDisplay(display),
       mainEncoder(encoder),
+      configMenu(config),
       goBack(onGoBack),
       goToConfig(onGoToConfig),
-      menuList(LIST, LIST_LENGTH) {
+      menuList(formattedList, LIST_LENGTH) {
     }
 
     void update() {
+      for(uint8_t i = 0; i < LIST_LENGTH; i++) {
+        if (i == 2) {
+          formattedList[i] = LIST[i] + ": " + configMenu.temperature + "c " + configMenu.minutes+"m";
+        } else {
+          formattedList[i] = LIST[i];
+        }
+      }
+
       if (mainEncoder.swUp && menuList.selectedIndex == 0) {
         goToConfig();
         return;
